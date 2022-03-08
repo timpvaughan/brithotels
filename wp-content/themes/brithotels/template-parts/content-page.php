45dd1b9f870572c54ab11aab6e923125e2bcc862
Tim Vaughan <tim.vaughan@crossorigin.co.uk>
2022-03-08 14:11:27 +0000

    CSS tweaks and Basic Page Template update

--- wp-content/themes/brithotels/template-parts/content-page.php
+++ wp-content/themes/brithotels/template-parts/content-page.php
@@ -9,40 +9,44 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php brithotels_post_thumbnail(); ?>
+<div class="row justify-content-center">
+	<div class="col-md-9">
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<?php brithotels_post_thumbnail(); ?>
 
-	<div class="entry-content">
-		<?php
-		the_content();
+			<div class="entry-content">
+				<?php
+				the_content();
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'brithotels' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'brithotels' ),
+					'after'  => '</div>',
+				) );
+				?>
+			</div><!-- .entry-content -->
 
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'brithotels' ),
-						array(
-							'span' => array(
-								'class' => array(),
+			<?php if ( get_edit_post_link() ) : ?>
+				<footer class="entry-footer">
+					<?php
+					edit_post_link(
+						sprintf(
+							wp_kses(
+								/* translators: %s: Name of current post. Only visible to screen readers */
+								__( 'Edit <span class="screen-reader-text">%s</span>', 'brithotels' ),
+								array(
+									'span' => array(
+										'class' => array(),
+									),
+								)
 							),
-						)
-					),
-					get_the_title()
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
-</article><!-- #post-<?php the_ID(); ?> -->
+							get_the_title()
+						),
+						'<span class="edit-link">',
+						'</span>'
+					);
+					?>
+				</footer><!-- .entry-footer -->
+			<?php endif; ?>
+		</article><!-- #post-<?php the_ID(); ?> -->
+	</div>
+</div>
